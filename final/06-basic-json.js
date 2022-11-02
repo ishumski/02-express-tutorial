8b7250308b869fe0d06c56305b6d6c60c94df894
Ilya Shumski <ilya.shumski@exposit.com>
2022-11-02 16:21:03 +0300

    6-basic-json params|query string

--- final/06-basic-json.js
+++ final/06-basic-json.js
@@ -5,14 +5,16 @@ const app = express()
 const PORT = 5001
 
 app.get('/', (req, res) => {
-    // res.json(
-    //     [
-    //         {name: "ilia"},
-    //         {name: "susan"},
-    //         {name: "john"}
-    //     ]
-    // )
-    res.json(products)
+    res.send('<h1>Home page</h1><a href="/api/products">Show products</a>')
+})
+
+app.get('/api/products', (req, res)=>{
+    const productsInfo = products.map((product)=>{
+        const { name, id, desc } = product
+
+        return { name, id, desc }
+    })
+    res.json(productsInfo)
 })
 
 app.listen(PORT, () => {
