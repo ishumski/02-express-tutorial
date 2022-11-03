c8ba735c0ab1432f0c45b3e348550fe99f8cb26e
i.white <whalterwhite23@gmail.com>
2022-11-03 20:40:34 +0300

    11-methods

--- final/11-methods.js
+++ final/11-methods.js
@@ -4,6 +4,13 @@ const {people} = require('./data')
 const PORT = 5001
 const app = express()
 
+app.use(express.static('methods-public'))
+
+//parse form data
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.json())
+
 app.get('/api/people', (req, res) => {
     res
         .status(200)
@@ -12,6 +19,47 @@ app.get('/api/people', (req, res) => {
                 success: true,
                 data: people
             })
+})
+
+app.post('/api/people', (req, res) => {
+    const {name} = req.body
+    if (!name) {
+        return res.status(400).json({success: false, msg: 'Please, provide name value'})
+    }
+    res.status(201).json({success: true, person: name})
+})
+
+app.post('/login', (req, res) => {
+    const {name} = req.body
+    if (name) {
+        res.status(200).send(`Welcome, ${name}`)
+    }
+    res.status(401).send('Enter your name')
+})
+
+app.put('/api/people/:id', (req, res) => {
+    const {id} = req.params
+    const {name} = req.body
+    const person = people.find((person) => person.id === Number(id))
+    if (!person) {
+        return res
+            .status(404)
+            .json({success: false, msg: `No person with id ${id}`})
+    }
+    const newPeople = people.map((person)=>{
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(201).json({success: true, data: newPeople})
+})
+
+app.delete('/api/people/:id', (req, res)=>{
+    const {id} = req.params
+    const filteredPeople = people.filter((person)=> person.id !== Number(id))
+    console.log(id)
+    res.status(201).json({success: true, data: filteredPeople})
 })
 
 app.listen(PORT, () => {
